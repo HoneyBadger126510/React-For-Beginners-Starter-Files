d539174182d2289ea1cbe975825605712654ae0b
jtawes <[jtawes510@gmail.com]>
2018-08-02 09:47:00 -0400

    changes up to lesson 19

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,12 +4,25 @@ import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
 import Fish from './Fish';
+import base from '../base';
 
 class App extends React.Component {
 	state = {
 		fishes: {},
 		order: {}
 	};
+
+	componentDidMount() {
+		const { params } = this.props.match;
+		this.ref = base.syncState(`${params.storeId}/fishes`, {
+			context: this,
+			state: 'fishes'
+		});
+	}
+
+	componentWillUnmount() {
+		base.removeBinding(this.ref);
+	}
 
 	addFish = (fish) => {
 		// 1. Take a copy of the existing state
@@ -41,19 +54,19 @@ class App extends React.Component {
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market"/>
 					<ul className="fishes">
-						{Object.keys(this.state.fishes).map(key => 
-							<Fish 
-								key={key} 
+						{Object.keys(this.state.fishes).map(key =>
+							<Fish
+								key={key}
 								index={key}
-								details={this.state.fishes[key]} 
-								addToOrder={this.addToOrder} 
+								details={this.state.fishes[key]}
+								addToOrder={this.addToOrder}
 							/>)}
 					</ul>
 				</div>
-				<Order />
-				<Inventory 
-					addFish={this.addFish} 
-					loadSampleFishes={this.loadSampleFishes} 
+				<Order fishes={this.state.fishes} order={this.state.order}/>
+				<Inventory
+					addFish={this.addFish}
+					loadSampleFishes={this.loadSampleFishes}
 				/>
 			</div>
 		)
